3ec54e976fc4aa61153d38fd5b455756130f1690
Arvydas <azegaspa@gmail.com>
2025-02-18 07:30:20 +0200

    add a database context, register it
    the database context is the main class that coordinates Entity Framework functionality for a data model
    
    register context with the DI (dependency injection) container. The container provides the service to controllers.
    
    specifying that we will use an in-memory database

---
+++ api/Models/CheckContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api.Models;
+
+public class CheckContext : DbContext
+{
+    public CheckContext(DbContextOptions<CheckContext> options)
+        : base(options)
+    { 
+    }
+
+    public DbSet<CheckItem> CheckItems { get; set; } = null!;
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CheckContext>(opt =>
+    opt.UseInMemoryDatabase("Gaidys"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
